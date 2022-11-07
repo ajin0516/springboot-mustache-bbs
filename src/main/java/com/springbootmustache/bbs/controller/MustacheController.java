30732f68d0ce9ddcd733b69b90692b2ea06465cb
ajin0516 <dkwls7432@naver.com>
2022-11-07 13:01:30 +0900

    [SpringBoot Mustache] GetMapping (username,id)

---
+++ src/main/java/com/springbootmustache/bbs/controller/MustacheController.java
@@ -0,0 +1,23 @@
+package com.springbootmustache.bbs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class MustacheController {
+
+    @GetMapping(value = "/hi")
+    public String mustacheCon(Model model) {
+        model.addAttribute("username", "jin"); // view에 값을 넘기겠다
+        return "greetings"; // greetings라는 이름의 view리턴
+    }
+
+    @GetMapping(value = "/hi/{id}")
+    public String mustacheCon(@PathVariable String id, Model model) {
+        model.addAttribute("username", " jin");
+        model.addAttribute("id", id);
+        return "greetings";
+    }
+}
